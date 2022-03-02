b17b4150b027d5bef14dfa76f502d0ca6256bc4d
Jiele Ye <yejiele1983@hotmail.com>
2022-03-02 15:36:15 +0800

    core:0.0.2;core-ui:0.0.4

---
+++ install-core.sh
@@ -0,0 +1,15 @@
+if [ -z "$1" ]; then
+	echo empty file
+	exit 1
+else
+	echo -Dfile=$1
+fi
+
+if [ -z "$2" ]; then
+	echo empty version
+	exit 2
+else
+	echo -Dversion=$2
+fi
+
+mvn install:install-file -DgroupId=com.jieleye.android.library -DartifactId=core -Dversion=$2 -Dfile=$1 -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
